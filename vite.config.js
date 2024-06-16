29507bbee98e472a9dd1a96eb800efc5ce883c8b
wszqj <3209494397@qq.com>
2024-06-16 14:16:30 +0800

    第一次提交

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://efibj9.natappfree.cc',
+        target: 'http://sv4d8c.natappfree.cc',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
